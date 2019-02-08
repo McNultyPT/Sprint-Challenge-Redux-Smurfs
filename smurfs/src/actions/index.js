8c468aae468ea3e9bc9cf9692a7840c67d6cc0c1
McNultyPT <mcnultypt@gmail.com>
2019-02-08 08:34:14 -0800

    writing action creator to get smurf

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,7 +1,20 @@
+import axios from 'axios';
+
+export const FETCH_SMURF_START = 'FETCH_SMURF_START';
+export const FETCH_SMURF_SUCCESS = 'FETCH_SMURF_SUCCESS';
+export const FETCH_SMURF_ERROR = 'FETCH_SMURF_ERROR';
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+
+export const getSmurf = () => dispatch => {
+  dispatch({ type: FETCH_SMURF_START });
+  axios
+    .get('http://localhost:3333/smurfs')
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+};
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
