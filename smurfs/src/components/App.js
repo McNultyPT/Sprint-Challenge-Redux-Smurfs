6102f82df1bd00570b7558241289a88992f3955c
McNultyPT <mcnultypt@gmail.com>
2019-02-08 08:45:57 -0800

    connecting App component

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { getSmurf } from '../actions';
 import './App.css';
+
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
  Just remember, `how do I `connect` my components to redux?`
  `How do I ensure that my component links the state to props?`
  */
+
 class App extends Component {
   render() {
     return (
@@ -19,4 +24,10 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  smurfs: state.smurfs,
+  fetchingSmurfs: state.fetchingSmurfs,
+  error: state.error
+});
+
+export default connect( mapStateToProps, { getSmurf })(App);
