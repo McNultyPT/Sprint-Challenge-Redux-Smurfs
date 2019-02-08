22d5a005de2cc63d3d5c65be814b8f46e6858c70
McNultyPT <mcnultypt@gmail.com>
2019-02-08 08:56:58 -0800

    mapping through smurfs array and returning smurf data from server

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -19,10 +19,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        {this.props.smurfs.map(smurf => {
+          return (
+            <div>
+              <p>{smurf.name}</p>
+              <p>{smurf.age}</p>
+              <p>{smurf.height}</p>
+            </div>
+          )
+        })}
       </div>
     );
   }
